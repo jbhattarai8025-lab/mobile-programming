5fa49b7e720911ad5440b322bb9cd49c086025b1
jbhattarai8025-lab <j.bhattarai.8025@westcliff.edu>
2026-09-10 07:45:23 +0545

    Calculator

--- javascript.js
+++ javascript.js
@@ -4,3 +4,15 @@ function sum() {
     let sum = number1 + number2;
     document.getElementById("result").innerHTML = "Result: " + sum;
 }
+function subtract() {
+    let number1 = Number(document.getElementById("number1").value);
+    let number2 = Number(document.getElementById("number2").value);
+    let difference = number1 - number2;
+    document.getElementById("result").innerHTML = "Result: " + difference;
+}
+function multiply() {
+    let number1 = Number(document.getElementById("number1").value);
+    let number2 = Number(document.getElementById("number2").value);
+    let product = number1 * number2;
+    document.getElementById("result").innerHTML = "Result: " + product;
+}
